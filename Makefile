4c34176474618f6d351e5dbeb6d4a470ad3aa29d
Christian Dreher <cdreher@prophesee.ai>
2020-03-23 15:07:25 +0100

    Add psee-spi-get

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Makefile to build i2c test for Prophesee systems.
 CFLAGS= -Wall
-BINARIES= psee-i2c-get psee-i2c-set psee-spi-set
+BINARIES= psee-i2c-get psee-i2c-set psee-spi-get psee-spi-set
 DESTDIR= usr/bin
 
 all: $(BINARIES)
@@ -14,6 +14,9 @@ psee-i2c-get: psee-i2c-get.o
 psee-i2c-set: psee-i2c-set.o
 	$(CC) $(LDFLAGS) -o $@ $^
 
+psee-spi-get: psee-spi-get.o
+	$(CC) $(LDFLAGS) -o $@ $^
+
 psee-spi-set: psee-spi-set.o
 	$(CC) $(LDFLAGS) -o $@ $^
 
@@ -21,9 +24,10 @@ install: $(BINARIES)
 	install -d $(DESTDIR)
 	install -m 0755 psee-i2c-get $(DESTDIR)
 	install -m 0755 psee-i2c-set $(DESTDIR)
+	install -m 0755 psee-spi-get $(DESTDIR)
 	install -m 0755 psee-spi-set $(DESTDIR)
 
 clean :
 	rm -f $(BINARIES)
-	rm -f psee-i2c-get.o psee-i2c-set.o psee-spi-set.o
+	rm -f psee-i2c-get.o psee-i2c-set.o psee-spi-get.o psee-spi-set.o
 
---
+++ psee-spi-get.c
@@ -0,0 +1,137 @@
+/*******************************************************************
+ * File : psee-spi-set.c                                           *
+ *                                                                 *
+ * Copyright: (c) 2020 Prophesee                                   *
+ *******************************************************************/
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <endian.h>
+#include <linux/spi/spidev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define MAX_READ 16
+
+static void print_usage(char *exec_name) {
+	printf("usage: %s [options] SPI_DEV REGISTER [NDATA] \n"
+		"SPI_DEV: complete path, such as /dev/spidev1.0\n"
+		"REGISTER: address of the first register to be read\n"
+		"NDATA: number of 32-bits registers to read (default: 1)\n"
+		"options:\n"
+		"\t-n:\tdry run: don't actually write the memory\n"
+		"\t-h:\tdisplay this message and quit with success\n",
+		exec_name);
+}
+
+int main(int argc, char* argv[])
+{
+	char* spi_dev_name;
+	int spi_dev;
+	struct spi_ioc_transfer xfer[2];
+	uint32_t reg_addr;
+	int ndata = 1;
+	uint32_t buffer[MAX_READ];
+	/* for getopt */
+	int opt;
+	int dry = 0;
+	int i;
+	/* for strtol */
+	char* endptr;
+
+	/* options */
+	while ((opt = getopt(argc, argv, "nh")) != -1) {
+		switch (opt) {
+		case 'n':
+			dry = 1;
+			break;
+		case 'h':
+			print_usage(argv[0]);
+			return EXIT_SUCCESS;
+			break;
+		default:
+			print_usage(argv[0]);
+			return EXIT_FAILURE;
+			break;
+		}
+	}
+
+	/* check for enough mandatory arguments */
+	if (optind + 3 >= argc) {
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	/* SPI_DEV */
+	spi_dev_name = argv[optind];
+	if ((spi_dev = open(spi_dev_name,O_RDWR)) < 0)
+	{
+		printf("Failed to open the bus: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	optind++;
+
+	/* REGISTER */
+	errno = 0;
+	reg_addr = strtol(argv[optind], &endptr, 0);
+	if (errno)
+	{
+		printf("Failed to parse reg address: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	buffer[0] = htobe32(reg_addr);
+	optind++;
+
+	/* NDATA */
+	errno = 0;
+	if (optind != argc)
+		ndata = strtol(argv[optind], &endptr, 0);
+	if (errno)
+	{
+		printf("Failed to parse NDATA: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	if ((ndata <= 0) || (ndata > MAX_READ))
+	{
+		printf("Can't transfer %d data\n", ndata);
+		return EXIT_FAILURE;
+	}
+	optind++;
+
+	/* check for spare arguments */
+	if (optind != argc) {
+		printf("Too many arguments\n");
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	/* Actual program starts here */
+
+	memset(xfer, 0, sizeof xfer);
+	xfer[0].tx_buf = (uint64_t)&buffer[0];
+	xfer[0].len = sizeof(buffer[0]);
+	xfer[0].delay_usecs = 1; //need to wait 64 sensor clk before read
+
+	xfer[1].rx_buf = (uint64_t)&buffer[0];
+	xfer[1].len = ndata*sizeof(buffer[0]);
+
+	if (!dry && (ioctl(spi_dev, SPI_IOC_MESSAGE(2), xfer)!=0))
+	{
+		printf("Failed to read on the spi bus: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	printf("SPI read result: %s\n", strerror(errno));
+	for (i = 0; i < ndata; i++)
+	{
+		printf("data[%d] = 0x%08x\n", i, be32toh(buffer[i]));
+	}
+
+	return EXIT_SUCCESS;
+}
